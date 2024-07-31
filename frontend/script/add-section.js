0e4d9246d5a1615c8a707564224c9fab9efeab62
Himansu Naik <naikhimansu546@gmail.com>
2024-07-31 14:43:32 +0530

    add styles and script folder with organize files

---
+++ frontend/script/add-section.js
@@ -0,0 +1,23 @@
+const skillEditorInput = document.querySelector("#skill-input");
+        const redirectBtn = document.querySelector("#redirect-arrow-icon");
+    	const submitBtn = document.querySelector("#submit-btn");
+
+
+    	submitBtn.addEventListener("click", () => {
+    		if (skillEditorInput.value !== "") {
+    		fetch("https://fullstack-profile-store-2.onrender.com/add-section", {
+    			method: "post",
+    			headers: {"Content-Type": "application/json"},
+    			body: JSON.stringify({name: skillEditorInput.value})
+    		})
+    		.then(res => res.json())
+    		.then(users => {
+    			skillEditorInput.value = ''
+    			skillEditorInput.focus();
+    		})
+
+    	}})
+
+    	redirectBtn.addEventListener("click", () => {
+    		window.location = "/";
+    	})
---
+++ frontend/script/edit-skills-section.js
@@ -0,0 +1,57 @@
+const skillsEditContainer = document.querySelector("#skills-edit-container");
+    	const redirectBtn = document.querySelector("#redirect-arrow-icon");
+
+         fetch("https://fullstack-profile-store-2.onrender.com/users")
+         .then(res => res.json())
+         .then((users, i) => {
+            users.forEach((user, j) => { 
+                 const delContainer = document.createElement("div");
+                 skillsEditContainer.appendChild(delContainer);
+                 delContainer.classList.add("del-container-style")
+        
+                 const skillName = document.createElement("h3");
+                 delContainer.appendChild(skillName);
+                 skillName.textContent = user.name;
+                 skillName.classList.add("skills-name");
+
+                 const inputContainer = document.createElement("input");
+                 delContainer.appendChild(inputContainer);
+                 inputContainer.classList.add("input-container-style");
+
+
+                 const updateBtn = document.createElement("button");
+                 delContainer.appendChild(updateBtn);
+                 updateBtn.textContent = "Update";
+                 updateBtn.style.display = "inline-block"
+
+
+                 updateBtn.addEventListener("click", () => {
+                    if (inputContainer.value !== "") {  
+                    fetch("https://fullstack-profile-store-2.onrender.com/users", {
+                        method: "put",
+                        headers: {"Content-Type": "application/json"},
+                        body: JSON.stringify({id: user.id, name: inputContainer.value})
+                    })
+                    window.location.reload()
+                    }
+                 })
+
+                        const delBtn = document.createElement("button");
+                        delContainer.appendChild(delBtn);
+                        delBtn.textContent = "Delete";
+                    delBtn.addEventListener("click", () => {
+                        fetch("https://fullstack-profile-store-2.onrender.com/users", {
+                            method: "delete",
+                            headers: {"Content-Type": "application/json"},
+                            body: JSON.stringify({id : user.id})
+                        })
+                        window.location.reload()
+                    })
+                })
+            
+
+         })
+
+        redirectBtn.addEventListener("click", () => {
+            window.location = "/";
+        })
