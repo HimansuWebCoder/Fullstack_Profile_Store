dc779cadadf94c7740fc08a02744c968e2776905
himansu naik <naikhimansu546@gmail.com>
2024-08-19 11:54:08 +0530

    updated env

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,32 +1,29 @@
 const knex = require("knex");
 
-// const db = knex({
-//   client: 'pg',
-//   connection: {
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: { rejectUnauthorized: false },
-//     host: process.env.DATABASE_HOST,
-//     port: 5432,
-//     // user: process.env.DATABASE_USER,
-//     user: "profile_store_database",
-//     // password: process.env.DATABASE_PW,
-//     password: "V6YpJ9fuRAimvbTgDZ3JAke22WKKMUGX",
-//     // database: process.env.DATABASE_DB,
-//     database: "profile_store_s11h"
-//   },
-// });
-
 const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
-    host: "dpg-cqhhdaiju9rs738l69g0-a.oregon-postgres.render.com",
+    host: process.env.DATABASE_HOST,
     port: 5432,
-    user: "profile_store_database",
-    password: "V6YpJ9fuRAimvbTgDZ3JAke22WKKMUGX",
-    database: "profile_store_s11h"
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB,
   },
 });
+
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: { rejectUnauthorized: false },
+//     host: "dpg-cqhhdaiju9rs738l69g0-a.oregon-postgres.render.com",
+//     port: 5432,
+//     user: "profile_store_database",
+//     password: "V6YpJ9fuRAimvbTgDZ3JAke22WKKMUGX",
+//     database: "profile_store_s11h"
+//   },
+// });
 
 module.exports = db;
