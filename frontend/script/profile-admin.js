d6e352b63b85fa384c39e054051e11f0e4318d59
himansu naik <naikhimansu546@gmail.com>
2024-09-07 06:40:19 +0530

    updated databse

--- frontend/script/profile-admin.js
+++ frontend/script/profile-admin.js
@@ -1,108 +1,106 @@
-      const profileContainer = document.querySelector("#profile-container");
-    	const profileImg = document.querySelector("#profile-image");
-    	const profileContentContainer = document.querySelector("#profile-content-container");
-    	const profileHeader = document.querySelector("#profile-header");
-    	const profileIntro = document.querySelector("#profile-intro");
-    	const profileEditBtn = document.querySelector(".profile-edit-icon");
-    	const addSectionBtn = document.querySelector("#add-section");
-    	const loader = document.querySelector(".loader");
+const profileContainer = document.querySelector("#profile-container");
+const profileImg = document.querySelector("#profile-image");
+const profileContentContainer = document.querySelector(
+  "#profile-content-container",
+);
+const profileHeader = document.querySelector("#profile-header");
+const profileIntro = document.querySelector("#profile-intro");
+const profileEditBtn = document.querySelector(".profile-edit-icon");
+const addSectionBtn = document.querySelector("#add-section");
+const loader = document.querySelector(".loader");
 
-      const uploadProfilePhoto = document.querySelector("#upload-profile-photo");
-    	// By Default name, passion/profession is there you can edit this according to your name and passion/profession
+const uploadProfilePhoto = document.querySelector("#upload-profile-photo");
+// By Default name, passion/profession is there you can edit this according to your name and passion/profession
 
+function showProfile() {
+  // console.log("Loading...");
+  // profileContainer.style.display = "none";
+  // loader.style.display = "block";
 
-         function showProfile() {
-              // console.log("Loading...");
-         	profileContainer.style.display = "none";
-         	loader.style.display = "block";
+  let isLoaded = false;
 
-              let isLoaded = false;
-
-                fetch("https://fullstack-profile-store-2.onrender.com/profile")
-                  .then(res => res.json())
-                  .then(users => {
-                    if (users.length) {
-                      users.forEach(user => {
-                        profileHeader.textContent = user.name;
-                        profileIntro.textContent = user.passion;
-                        isLoaded = true; // Data loaded successfully
-                      });
-                    }
-                  })
-                  .finally(() => {
-                    if (isLoaded) {
-                    	profileContainer.style.display = "block";
-                    	loader.style.display = "none"
-                    }
-                  });
-
-                   fetch("https://fullstack-profile-store-2.onrender.com/view")
-                  .then(res => res.json())
-                  .then(users => {
-                        console.log(users[users.length-1].image);
-                        // profileImg.src = users[users.length-1].image;
-                        // profileImg.src = '/' + users[users.length-1].image;
-                        profileImg.src = users[users.length-1].image;
-                  })
-                  .finally(() => {
-                    if (isLoaded) {
-                      profileContainer.style.display = "block";
-                      loader.style.display = "none"
-                    }
-                  });
-            }
-
-
-         showProfile()
-
-    	fetch("https://fullstack-profile-store-2.onrender.com/users")
-    	.then(res => res.json())
-    	.then((users, i) => {
-            // All skills container box    		
-    		        const skillDiv = document.createElement("div");
-    		        profileContainer.appendChild(skillDiv);
-    		        skillDiv.textContent = "Skills";
-    		        skillDiv.classList.add("skills-intro-style")
-
-    		  users.forEach((user, i) => {
-    		 	// One skill container box
-    		 	      const skillContainer = document.createElement("div");
-    		 	      profileContainer.appendChild(skillContainer);
-    		 	      skillContainer.textContent = users[i].name;
-    		 	      skillContainer.classList.add("skills-container");
-                
-    		 	      const editSkillBtn = document.createElement("button");
-                skillContainer.appendChild(editSkillBtn);
-                editSkillBtn.textContent = "Edit"
-                editSkillBtn.classList.add("edit-skills-btn-style")
-                editSkillBtn.setAttribute("class", "edit-btns")
-
-                const delSkillBtn = document.createElement("button");
-                skillContainer.appendChild(delSkillBtn);
-                delSkillBtn.textContent = "Delete"
-                delSkillBtn.classList.add("edit-skills-btn-style")
-                delSkillBtn.setAttribute("class", "del-btns")
-
-
-              editSkillBtn.addEventListener("click", () => {
-                  window.location.href = `skill_edit/${user.id}/edit`
-              })
-
-              delSkillBtn.addEventListener("click", () => {
-                  window.location.href = `skill_delete/${user.id}/delete`
-              })
-
-    	})
+  fetch("https://fullstack-profile-store-2.onrender.com/profile")
+    .then((res) => res.json())
+    .then((users) => {
+      if (users.length) {
+        users.forEach((user) => {
+          profileHeader.textContent = user.name;
+          profileIntro.textContent = user.passion;
+          isLoaded = true; // Data loaded successfully
+        });
+      }
     })
+    .finally(() => {
+      if (isLoaded) {
+        profileContainer.style.display = "block";
+        loader.style.display = "none";
+      }
+    });
 
-        profileEditBtn.addEventListener("click", () => {
-        	window.location = "edit-profile";
-        }) 
+  fetch("https://fullstack-profile-store-2.onrender.com/view")
+    .then((res) => res.json())
+    .then((users) => {
+      console.log(users[users.length - 1].image);
+      // profileImg.src = users[users.length-1].image;
+      // profileImg.src = '/' + users[users.length-1].image;
+      profileImg.src = users[users.length - 1].image;
+    })
+    .finally(() => {
+      if (isLoaded) {
+        profileContainer.style.display = "block";
+        loader.style.display = "none";
+      }
+    });
+}
 
-        uploadProfilePhoto.addEventListener("click", () => {
-          window.location = "imageUpload";
-        })
+showProfile();
 
-        addSectionBtn.addEventListener("click", () => {
-        	window.location = "add-section";
-        })
+fetch("https://fullstack-profile-store-2.onrender.com/users")
+  .then((res) => res.json())
+  .then((users, i) => {
+    // All skills container box
+    const skillDiv = document.createElement("div");
+    profileContainer.appendChild(skillDiv);
+    skillDiv.textContent = "Skills";
+    skillDiv.classList.add("skills-intro-style");
+
+    users.forEach((user, i) => {
+      // One skill container box
+      const skillContainer = document.createElement("div");
+      profileContainer.appendChild(skillContainer);
+      skillContainer.textContent = users[i].name;
+      skillContainer.classList.add("skills-container");
+
+      const editSkillBtn = document.createElement("button");
+      skillContainer.appendChild(editSkillBtn);
+      editSkillBtn.textContent = "Edit";
+      editSkillBtn.classList.add("edit-skills-btn-style");
+      editSkillBtn.setAttribute("class", "edit-btns");
+
+      const delSkillBtn = document.createElement("button");
+      skillContainer.appendChild(delSkillBtn);
+      delSkillBtn.textContent = "Delete";
+      delSkillBtn.classList.add("edit-skills-btn-style");
+      delSkillBtn.setAttribute("class", "del-btns");
+
+      editSkillBtn.addEventListener("click", () => {
+        window.location.href = `skill_edit/${user.id}/edit`;
+      });
+
+      delSkillBtn.addEventListener("click", () => {
+        window.location.href = `skill_delete/${user.id}/delete`;
+      });
+    });
+  });
+
+profileEditBtn.addEventListener("click", () => {
+  window.location = "edit-profile";
+});
+
+uploadProfilePhoto.addEventListener("click", () => {
+  window.location = "imageUpload";
+});
+
+addSectionBtn.addEventListener("click", () => {
+  window.location = "add-section";
+});
