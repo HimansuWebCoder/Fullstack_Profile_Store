67d6b223acdfc1b10a5c3af43acfabb3e74b0e0d
himansu naik <naikhimansu546@gmail.com>
2024-09-09 22:21:04 +0530

    testing register

--- backend/controllers/login/register.js
+++ backend/controllers/login/register.js
@@ -2,6 +2,7 @@ const db = require("../../models/db");
 
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
+  const passion = "Web Development";
   console.log(req.body);
   if (!email || !name || !password) {
     return res.status(400).json("incorrect form submission");
@@ -22,6 +23,7 @@ const handleRegister = (req, res, db, bcrypt) => {
           .insert({
             email: loginEmail[0].email,
             name: name,
+            passion: passion,
           })
           .then((user) => {
             res.json(user[0]);
