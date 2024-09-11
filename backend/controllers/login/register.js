3126ac561491278ed98da2d596f031ac8ea5786a
himansu naik <naikhimansu546@gmail.com>
2024-09-11 08:20:19 +0530

    updated resolved ; this issue

--- backend/controllers/login/register.js
+++ backend/controllers/login/register.js
@@ -19,28 +19,27 @@ const handleRegister = (req, res, db, bcrypt) => {
       .returning("email")
       .then((loginEmail) => {
         return trx
-        .select('id') // Assuming you need to select the image ID
-        .from('images')
-        .where({image: image})
-        .limit(1); // Limit to 1 image
-        })
-         .then((images) => {
-          const imageId = images[0]?.id;
-            return trx("profile")
-                .returning("*")
-                .insert({
-                  email: loginEmail[0].email,
-                  name: name,
-                  passion: passion,
-                  image: imageId // Add the image ID to the profile
-                });
-           })
-          .then((profile) => {
-            return trx.commit() // Commit should be returned to properly handle the promise
-              .then(() => {
-                 res.json(profile[0]); // Send response after commit
-               });
+          .select("id") // Assuming you need to select the image ID
+          .from("images")
+          .where({ image: image })
+          .limit(1); // Limit to 1 image
+      })
+      .then((images) => {
+        const imageId = images[0]?.id;
+        return trx("profile").returning("*").insert({
+          email: loginEmail[0].email,
+          name: name,
+          passion: passion,
+          image: imageId, // Add the image ID to the profile
+        });
+      })
+      .then((profile) => {
+        return trx
+          .commit() // Commit should be returned to properly handle the promise
+          .then(() => {
+            res.json(profile[0]); // Send response after commit
           });
+      })
       .catch((err) => {
         console.log("transaction failed", err);
         trx.rollback();
