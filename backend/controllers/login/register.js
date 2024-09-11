114e9e2e2c3045c59bc8ed3a7e4d191099626726
himansu naik <naikhimansu546@gmail.com>
2024-09-11 08:14:01 +0530

    updated image insert in register

--- backend/controllers/login/register.js
+++ backend/controllers/login/register.js
@@ -18,18 +18,29 @@ const handleRegister = (req, res, db, bcrypt) => {
       .into("login")
       .returning("email")
       .then((loginEmail) => {
-        return trx("profile")
-          .returning("*")
-          .insert({
-            email: loginEmail[0].email,
-            name: name,
-            passion: passion,
-          })
-          .then((user) => {
-            res.json(user[0]);
+        return trx
+        .select('id') // Assuming you need to select the image ID
+        .from('images')
+        .where(image: image)
+        .limit(1); // Limit to 1 image
+        })
+         .then((images) => {
+          const imageId = images[0]?.id;
+            return trx("profile")
+                .returning("*")
+                .insert({
+                  email: loginEmail[0].email,
+                  name: name,
+                  passion: passion,
+                  image: imageId // Add the image ID to the profile
+                });
+           })
+          .then((profile) => {
+            return trx.commit() // Commit should be returned to properly handle the promise
+              .then(() => {
+                 res.json(profile[0]); // Send response after commit
+               });
           });
-      })
-      .then(trx.commit)
       .catch((err) => {
         console.log("transaction failed", err);
         trx.rollback();
