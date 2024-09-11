bea8283eb91456c7672d577e88eb49723a06d77a
himansu naik <naikhimansu546@gmail.com>
2024-09-11 10:01:43 +0530

    updated register

--- backend/controllers/login/register.js
+++ backend/controllers/login/register.js
@@ -55,3 +55,65 @@ const handleRegister = (req, res, db, bcrypt) => {
 module.exports = {
   handleRegister,
 };
+
+// const db = require("../../models/db");
+
+// const handleRegister = (req, res, db, bcrypt) => {
+//   const { email, name, password } = req.body;
+//   const passion = "Web Development";
+//   const image =
+//     "https://res.cloudinary.com/dtiasevyl/image/upload/v1725802853/IMG_20240814_191245.jpg";
+
+//   if (!email || !name || !password) {
+//     return res.status(400).json("incorrect form submission");
+//   }
+
+//   const hash = bcrypt.hashSync(password);
+
+//   db.transaction((trx) => {
+//     trx
+//       .insert({
+//         hash: hash,
+//         email: email,
+//       })
+//       .into("login")
+//       .returning("email")
+//       .then((loginEmail) => {
+//         return trx
+//           .select("id")
+//           .from("images")
+//           .where({ image: image })
+//           .limit(1);
+//       })
+//       .then((images) => {
+//         if (!images || images.length === 0) {
+//           throw new Error("Image not found");
+//         }
+//         const imageId = images[0].id;
+//         return trx("profile").returning("*").insert({
+//           email: loginEmail[0].email,
+//           name: name,
+//           passion: passion,
+//           image: imageId,
+//         });
+//       })
+//       .then((profile) => {
+//         return trx.commit() // Ensure trx.commit() is returned
+//           .then(() => {
+//             res.json(profile[0]); // Send response after commit
+//           });
+//       })
+//       .catch((err) => {
+//         console.log("Transaction failed", err);
+//         trx.rollback(); // Rollback the transaction in case of error
+//         res.status(400).json("unable to register"); // Send error response
+//       });
+//   }).catch((err) => {
+//     console.log("Transaction error", err);
+//     res.status(400).json("unable to register");
+//   });
+// };
+
+// module.exports = {
+//   handleRegister,
+// };
