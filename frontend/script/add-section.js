cf9b15fffd7324edbb7869a348dc6f6da26179e5
himansu naik <naikhimansu546@gmail.com>
2024-09-12 12:59:51 +0530

    update post for user specific skill post

--- frontend/script/add-section.js
+++ frontend/script/add-section.js
@@ -2,25 +2,25 @@ const skillEditorInput = document.querySelector("#skill-input");
 const redirectBtn = document.querySelector("#redirect-arrow-icon");
 const submitBtn = document.querySelector("#submit-btn");
 
-submitBtn.addEventListener("click", () => {
-        if (skillEditorInput.value !== "") {
-                fetch(
-                        "https://fullstack-profile-store-2.onrender.com/profile/:profileId/skills",
-                        {
-                                method: "post",
-                                headers: { "Content-Type": "application/json" },
-                                body: JSON.stringify({
-                                        name: skillEditorInput.value,
-                                }),
-                        },
-                )
-                        .then((res) => res.json())
-                        .then((users) => {
-                                skillEditorInput.value = "";
-                                skillEditorInput.focus();
-                        });
-        }
-});
+// submitBtn.addEventListener("click", () => {
+//         if (skillEditorInput.value !== "") {
+//                 fetch(
+//                         "https://fullstack-profile-store-2.onrender.com/profile/:profileId/skills",
+//                         {
+//                                 method: "post",
+//                                 headers: { "Content-Type": "application/json" },
+//                                 body: JSON.stringify({
+//                                         name: skillEditorInput.value,
+//                                 }),
+//                         },
+//                 )
+//                         .then((res) => res.json())
+//                         .then((users) => {
+//                                 skillEditorInput.value = "";
+//                                 skillEditorInput.focus();
+//                         });
+//         }
+// });
 
 submitBtn.addEventListener("click", () => {
         if (skillEditorInput.value !== "") {
