820bec5ae0e8cb142d2b8adcbffb8ec713b48537
himansu naik <naikhimansu546@gmail.com>
2024-09-14 16:13:58 +0530

    resolved stack dir mistake of upload in upload router

--- backend/routes/upload.router.js
+++ backend/routes/upload.router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { uploadPost, viewImgPost } = require("../controllers/upload.controller");
-const upload = require("./config/multerConfig");
+const upload = require("../config/multerConfig");
 const db = require("../config/db");
 const uploadRouter = express.Router();
 
