0011d63b3beefd6726152766d21731d11d9ea332
himansu naik <naikhimansu546@gmail.com>
2024-09-18 05:50:03 +0530

    updated image upload form

--- backend/routes/upload.router.js
+++ backend/routes/upload.router.js
@@ -5,7 +5,7 @@ const db = require("../config/db");
 const uploadRouter = express.Router();
 
 // UPLOAD POST
-uploadRouter.post("/", upload.single("profile_pic"), (req, res) => {
+uploadRouter.post("/", upload.single("image_uploads"), (req, res) => {
 	uploadPost(req, res, db);
 });
 
